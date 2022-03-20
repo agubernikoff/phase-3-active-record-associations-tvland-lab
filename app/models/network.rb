18c8575dcbe3a74bf16c8ab05789d068372865d2
alexander gubernikoff <agubernikoff@gmail.com>
2022-03-19 23:12:26 -0400

    completed assignment

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,9 @@
+require 'pry'
 class Network < ActiveRecord::Base
-  
+    has_many :shows
+
+    def sorry
+        # binding.pry
+        return "We're sorry about passing on John Mulaney's pilot"
+    end
 end
